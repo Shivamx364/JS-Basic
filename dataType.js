055b3bfab36b7deeb226be02d5e2f725e2f91f0c
Shivam kumar <Shivamx364@gmail.com>
2024-04-10 22:25:19 +0530

    ok

--- dataType.js
+++ dataType.js
@@ -1,2 +1,24 @@
 "use strict"; //treat all JS code as newer version
-alert(3+3);
+//alert(3+3); //node is in use not browser
+
+console.log(3+3);
+console.log("Add");
+
+let age=18
+let i=false
+let state;
+
+
+//number => 2 to power 53
+//bigint
+//string => true/false
+//null => standalone values
+//undedfined=>
+//symbol=>
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
---
+++ datatypoConver.js
@@ -0,0 +1,81 @@
+// let score=null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valIntNumber = Number(score)
+// console.log(typeof valIntNumber);
+// console.log(valIntNumber);
+
+//"33" => 33
+//"ss3" => NaN
+//true =1 fale =0
+//null=0 ""=0
+
+// let bool ="j"
+// let cbool= Boolean(bool)
+// console.log(typeof cbool);
+// console.log(cbool);
+
+//1 ,"t"=> true 
+//"",0=>false
+
+// let someNum=99
+// let stringNum=String(someNum)
+// console.log(typeof stringNum);
+// console.log(stringNum);
+
+// ******* Operation ***********
+
+let value = 3
+let negValue= -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1= "hello"
+let str2 = "Sk"
+// console.log(str1+str2);
+
+// console.log("1"+ 2);
+// console.log(1+2+"1"+ 2+2+2);
+// console.log(3+4*5%3);
+
+console.log(+true);
+
+let nm1, nm2, nm3;
+nm1=nm2=nm3=2+3
+
+// let gameCode=100
+// ++gameCode
+// console.log(gameCode++);
+// console.log(gameCode++);
+// console.log(gameCode++);
+
+// console.log(2>1);
+// console.log(2>=2);
+// console.log(2 =="02");
+
+// console.log(null>1);
+// console.log(null >= 0);
+// console.log(null =="0");
+
+// console.log(undefined>1);
+// console.log(undefined >= 0);
+// console.log(undefined =="0");
+
+//===
+
+console.log(2==="2");
+
+
+
+
+
+
+
