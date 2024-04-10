4ec8c216a2d24b5dd39fedad7b002d32a150ff5f
Shivam kumar <Shivamx364@gmail.com>
2024-04-10 20:45:47 +0530

    ok

---
+++ variable.js
@@ -0,0 +1,15 @@
+const id=123
+var pass="asd1323"
+let email="abc@gmail.com"
+city="Bhopal"
+
+//id=9  //not allowed
+
+if(id==123){
+    var pass="changge"
+}
+
+/*prefer not to use var 
+var have scope issue so use let */
+
+console.table([id,pass,email,city]);
